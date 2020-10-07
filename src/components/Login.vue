9e0c262d816dd46e22bee74167fd66a1b6b91ba5
Gerardo Lucero <gera_conecta@hotmail.com>
2020-10-07 11:12:22 -0600

    se agrego firebase y bootstrap

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+	<section>
+		
+	</section>
+</template>
+
+<script>
+import { firebase, auth, firestore } from '@/firebase'
+
+export default {
+	name: 'Login',
+
+	data(){
+		return {
+
+		}
+	},
+
+	methods: {
+
+	}
+}
+</script>
